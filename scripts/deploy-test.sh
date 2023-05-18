4a8be1523b486ac7386ee28aa0bb0564f71b1382
samgopher <samtech09@gmail.com>
2023-05-18 19:02:00 +0530

    updated deploy script

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -2,5 +2,5 @@
 ./scripts/build.sh
 7z a ./tusd_linux_amd64/tusd.zip ./tusd_linux_amd64/tusd
 scp ./tusd_linux_amd64/tusd.zip linode-st:~/goapps/temp/
-sh /home/samtech/Develop/scripts/deploy-test.sh 'tusd'
+sh ~/Develop/scripts/deploy-test.sh 'tusd'
 echo "Done"
